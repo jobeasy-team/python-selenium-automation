d3dc824224b25e0a377afdb55aee94ea90dd39b3
Nataly <natalycupo@outlook.com>
2019-10-08 20:42:23 -0400

    HW code shopping cart

--- features/steps/shopping_cart_code.py
+++ features/steps/shopping_cart_code.py
@@ -1,5 +1,6 @@
 from behave import given, when, then
 from selenium.webdriver.common.by import By
+from time import sleep
 
 
 @given('Open amazon page')
@@ -9,12 +10,15 @@ def open_amazon_page(context):
 
 @when('Click on shopping cart button')
 def click_on_shopping_cart(context):
-    context.driver.find_element(By.ID, 'nav-cart')
+    context.driver.find_element(By.ID, 'nav-cart').click()
 
 
 @then('Verify the cart is empty')
 def verify_result(context):
-    result_text = context.driver.find_element(By.XPATH, "//div[@class='a-row sc-cart-header']").text
-    assert "Your Shopping Cart is empty." in result_text, f"Expected text is Your Shopping Cart is empty., but got {result_text}"
+    result = context.driver.find_element(By.XPATH, "//h1[@class = 'sc-empty-cart-header']").text
+    assert "empty" in result, f"Expected text is empty, but got {result}"
+
+
+
 
 
