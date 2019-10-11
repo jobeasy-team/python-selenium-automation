193986ee584131868206948f2782e17200fefa5f
Maria Drogaytseva <zzohamadro@Marias-MacBook-Pro.local>
2019-10-11 06:20:40 -0700

    Practice with locators HW2

---
+++ practice_with_locators.py
@@ -0,0 +1,23 @@
+
+#amazon_logo = driver.find_element(By.CLASS_NAME, 'a-icon a-icon-logo')
+
+#email_field = driver.find_element(By.ID 'ap_email')
+
+#continue_button = driver.find_element(By.ID, 'continue')
+
+#need_help_link = driver.find_element(By.CLASS_NAME, 'a-expander-header a-declarative a-expander-inline-header a-link-expander')
+
+#forgot_your_password_link = driver.find_element(By.ID, 'auth-fpp-link-bottom')
+
+#other_issues_with_sign_in_link = driver.find_element(By.ID, 'ap-other-signin-issues-link')
+
+#create_your_amazon_account_button = driver.find_element(By.ID, 'createAccountSubmit')
+
+#conditions_of_use_link = driver.find_element(By.LINK_TEXT, 'Conditions of Use')
+
+#giprivacy_notice_link = driver.find_element(By.LINK_TEXT, 'Privacy Notice')
+
+
+g
+
+
