bdff852c6999d09c0f475dd67905750ffdf0efa4
Maria Fedorova <maria.v.fedorova@gmail.com>
2020-01-25 22:22:37 -0800

    no message

---
+++ hw_2/hw_2_3.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('https://www.amazon.com/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_signin&switch_account=')
+
+
+from selenium.webdriver.common.by import By
+
+driver.find_element(By.XPATH, "//div[@id='authportal-main-section']//a[@class='a-expander-header a-declarative a-expander-inline-header a-link-expander']").click()
