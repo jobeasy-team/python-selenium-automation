ed47a0143dcb6238154d6adcd2d5f5d3788a1d57
Valli <rmvkdeals1@gmail.com>
2020-09-22 20:38:08 -0400

    added webdriver path

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('../../drivers/chromedriver_win32/chromedriver.exe')
 driver.maximize_window()
 
 # open the url
