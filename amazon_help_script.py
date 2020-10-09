d93d2db6a8e9bdf3c29d157c80f242b8cd0ec4d6
Valli <rmvkdeals1@gmail.com>
2020-10-08 21:40:52 -0400

    Amazon help and locator

---
+++ amazon_help_script.py
@@ -0,0 +1,19 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# init driver
+driver = webdriver.Chrome('../../drivers/chromedriver_win32/chromedriver.exe')
+driver.maximize_window()
+driver.implicitly_wait(5)
+
+# open the url
+driver.get(' https://www.amazon.com/gp/help/customer/display.html ')
+
+input_field = driver.find_element(By.ID, 'helpsearch').send_keys('Cancel order',Keys.ENTER)
+
+result = driver.find_element(By.XPATH, "//h1[text()='Cancel Items or Orders']")
+assert result.text == "Cancel Items or Orders", f'Error.Expected Cancel Items or Orders, but got(result.text)'
+
+driver.quit()
---
+++ locators.py
@@ -0,0 +1,17 @@
+#Locators
+#Amazon logo
+findelement(By.class('nav-logo-link'))
+#Continue button
+findelement(By.id('continue'))
+#Need help link
+findelement(By.class('a-expander-prompt'))
+#forgot your pasword
+findelement(By.class('a-link-normal'))
+#Other issue
+findelement(By.id('ap-other-signin-issues-link'))
+#Create your own amazon account
+findelement(By.id('createAccountSubmit'))
+#Conditios of use link
+findelement(By.target('_blank'))
+#privacy Notice Link
+findelement(By.rel('noopener'))
