ae5b97214eec87c92aaa9f72f54e161d0039cb91
twinkot <bkarpov86@gmail.com>
2021-07-01 07:57:13 -0700

    fist commit

---
+++ Algorightms HW2 #2.py
@@ -0,0 +1,16 @@
+
+#Unique Characters in String
+#Given a string, determine if it consists of all unique characters.
+#For example, the string 'abcde' has all unique characters and should return True.
+#The string 'aabcde' contains duplicate characters and should return False.
+#Hint: https://www.w3schools.com/python/python_sets.asp
+
+txt1 = {"abcde" "aabcde"}
+#print(txt1)
+
+print('aa' in txt1)
+print('cde' in txt1)
+print('abcde' in txt1)
+print('a' in txt1)
+print('abc' in txt1)
+print('' in txt1)
