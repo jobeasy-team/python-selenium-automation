9a792a20f32e0b9befa4168c19bc575921c24068
twinkot <bkarpov86@gmail.com>
2021-07-10 20:18:25 -0700

    New commit

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
+from app.app import App
 
 def browser_init(context):
     """
@@ -13,6 +14,8 @@ def browser_init(context):
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 10)
+
+    context.app = App(context.driver)
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
---
+++ pages/Sign_in_page.py
@@ -0,0 +1,8 @@
+from pages.base_page import Page
+from selenium.webdriver.common.by import By
+
+
+class Sign_in_link(Page):
+    def verify_sign_in_opened(self):
+        self.open_url(url='https://www.amazon.com/ap/signin')
+
