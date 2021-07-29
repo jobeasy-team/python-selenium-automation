a532e1b94549b64ad6fa09f58fc56b1f1cb27946
Jeffrey Smith <jasmith06902@gmail.com>
2021-07-29 11:14:17 -0400

    Jeffrey Smith HW4

---
+++ algoritm_homework/Algorithm HW5.py
@@ -0,0 +1,49 @@
+"""
+Even First
+Your input is an array of integers, and you have to reorder its entries so that the even entries appear first.
+ You are required to solve it without allocating additional storage (operate with the input array).
+Example: [7, 3, 5, 6, 4, 10, 3, 2] Return [6, 4, 10, 2, 7, 3, 5, 3]
+"""
+
+# def even_first(arr):
+#     next_even = 0
+#     next_odd = len(arr) - 1
+#
+#     while next_even < next_odd:
+#         if arr[next_even] % 2 == 0:
+#             next_even += 1
+#         else:
+#             arr[next_even], arr[next_odd] = arr[next_odd], arr[next_even]
+#             next_odd -= 1
+#
+# test_array = [7, 3, 5, 6, 4, 10, 3, 2]
+# print(test_array)
+# even_first(test_array)
+# print(test_array)
+
+
+
+"""
+Increment a Number
+Write a program that takes as input an array of digits encoding a nonnegative decimal integer 
+D and updates the array to represent the integer D + 1.
+For example, if the input is [1, 2, 9] then you should update the array to [1, 3, 0]
+"""
+
+def increment_number(arr):
+    arr[-1] += 1
+    for i in reversed(range(1, len(arr))):
+        if arr[i] != 10:
+            break
+        arr[i] = 0
+        arr[i - 1] += 1
+    if arr[0] == 10:
+        arr[0] = 1
+        arr.append(0)
+
+    return arr
+
+
+test_array = [9, 9, 9]
+print(test_array)
+print(increment_number(test_array))
