757962ce17226eb075ef6e940f981aa1b0d447d4
Ayaz <ayazbabayev12@gmail.com>
2021-12-05 20:50:30 -0800

    HW3 commited

--- HW3 - CSS Selectors for Amazon sign in page.py
+++ HW3 - CSS Selectors for Amazon sign in page.py
@@ -5,3 +5,29 @@ driver = webdriver.Chrome()
 #AMAZON LOGO
 driver.find_element(By.CSS_SELECTOR, "i[aria-label='Amazon']")
 
+#CREATE ACCOUNT
+driver.find_element(By.CSS_SELECTOR, "h1.a-spacing-small")
+
+#YOUR NAME
+driver.find_element(By.CSS_SELECTOR,"#ap_customer_name")
+
+#EMAIL
+driver.find_element(By.CSS_SELECTOR, "#ap_email")
+
+#PASSWORD
+driver.find_element(By.CSS_SELECTOR, "#ap_password")
+
+#RE-ENTER PASSWORD"
+driver.find_element(By.CSS_SELECTOR, "#ap_password_check")
+
+#CONTINUE (CREATE YOUR AMAZON ACCOUNT)
+driver.find_element(By.CSS_SELECTOR, "#continue")
+
+#CONDITIONS OF USE
+driver.find_element(By.CSS_SELECTOR, "a[href*='/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use']")
+
+#PRIVACY NOTICE
+driver.find_element(By.CSS_SELECTOR, "a[href*='/gp/help/customer/display.html/ref=ap_register_notification_privacy_notice']")
+
+#SIGN IN
+driver.find_element(By.CSS_SELECTOR, "a[href*='/ap/signin?openid']")
