e87be3f686c78c254a50885d4320647a79b92aab
Bobo <boboxu5858@gmail.com>
2021-12-12 00:09:34 -0500

    Homework 4 -Open Amazon bestSeller page (2)

--- features/steps/hw4_amazon_best_sellers.py
+++ features/steps/hw4_amazon_best_sellers.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
-from behave import given, then
+from selenium import webdriver
+from behave import given, when, then
 
 
 @given('Open Amazon bestSeller page')
@@ -19,3 +20,41 @@ def links_display(context, number_links):
     link_array = context.driver.find_elements(By.CSS_SELECTOR, "._p13n-zg-nav-tab-all_style_zg-tabs__EYPLq li")
     assert len(link_array) == int(number_links), f"Error, number of links is not {number_links}, but {len(link_array)}"
     print('Test Passed')
+
+
+@given('Open Amazon page')
+def open_amazon(context):
+    context.driver.get('https://www.amazon.com/')
+
+
+
+@when('Search for {search_item}')
+def search(context, search_item):
+    context.driver.find_element(By.ID, "twotabsearchtextbox").send_keys(search_item)
+
+
+@when('Click for search icon')
+def click_search_button(context):
+    context.driver.find_element(By.ID, "nav-search-submit-button").click()
+
+
+@when('Click on the first product')
+def click_first_prod(context):
+    context.driver.find_element(By.CSS_SELECTOR, "span.a-price-whole").click()
+
+
+@when('Click on Add to cart button')
+def click_add_cart_button(context):
+    context.driver.find_element(By.ID, "add-to-cart-button").click()
+
+
+@when('Open cart page')
+def open_cart(context):
+    context.driver.find_element(By.ID, "nav-cart-count").click()
+
+
+@then('Verify cart has {number} item(s)')
+def verify_cart_item(context, number):
+    cart_item_no= context.driver.find_element(By.ID, "nav-cart-count").text
+    assert int(cart_item_no) == int(number), f"error, real number is {cart_item_no}, not {number}"
+
--- features/tests/hw4amazon_best_sellers.feature
+++ features/tests/hw4amazon_best_sellers.feature
@@ -6,3 +6,14 @@ Feature: User can Open bestSellers on Amazon
   Given Open Amazon bestSeller page
     Then  bestseller page is shown
     Then  5 links display on the page
+
+
+  Scenario: User can add product to cart
+
+    Given Open Amazon page
+    When Search for glass
+    And Click for search icon
+    And Click on the first product
+    And Click on Add to cart button
+    And Open cart page
+    Then Verify cart has 1 item(s)
