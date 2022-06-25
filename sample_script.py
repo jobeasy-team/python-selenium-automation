1f5fd7d7af3908a949807c8f179d9cfef8eda311
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2022-06-25 11:01:23 -0700

    Path added

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='/Users/svetlanalevinsohn/JobEasy/11-python-selenium-automation/chromedriver')
 driver.maximize_window()
 
 # open the url
