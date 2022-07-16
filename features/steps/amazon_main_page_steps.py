12643c0271d6a7f3794966f669a6a6f7a3bf19e4
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2022-07-16 12:16:11 -0700

    class4

---
+++ features/steps/amazon_main_page_steps.py
@@ -0,0 +1,40 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+ORDERS_BTN = (By.ID, 'nav-orders')
+INPUT_FIELD = (By.ID, 'twotabsearchtextbox')
+SEARCH_ICON = (By.ID, 'nav-search-submit-button')
+HAM_MENU = (By.ID, 'nav-hamburger-menu')
+FOOTER_LINKS = (By.CSS_SELECTOR, 'table.navFooterMoreOnAmazon td a')
+
+
+@given('Open Amazon page')
+def open_amazon(context):
+    context.driver.get('https://www.amazon.com/')
+
+
+@when('Search for {search_word} on amazon')
+def search_product(context, search_word):
+    context.driver.find_element(*INPUT_FIELD).send_keys(search_word)
+    context.driver.find_element(*SEARCH_ICON).click()
+    sleep(1)
+
+
+@when('Click Orders')
+def click_orders(context):
+    context.driver.find_element(*ORDERS_BTN).click()
+
+
+@then('Verify hamburger menu is present')
+def verify_hamburger_menu_present(context):
+    context.driver.find_element(*HAM_MENU)
+
+
+@then('Verify {expected_amount} footer links are shown')
+def verify_footer_links_present(context, expected_amount):
+    expected_amount = int(expected_amount)  # '38' => 38
+    links = context.driver.find_elements(*FOOTER_LINKS)
+
+    assert len(links) == expected_amount, \
+        f'Expected {expected_amount} links but got {len(links)}'
---
+++ features/tests/amazon_search.feature
@@ -0,0 +1,27 @@
+# Created by svetlanalevinsohn at 7/9/22
+Feature: Amazon Product Search Tests
+
+  Scenario Outline: User can search for a product
+    Given Open Amazon page
+    When Search for <product> on amazon
+    Then User sees results for <search_result>
+   Examples:
+    |product      |search_result   |
+    |mug          |"mug"           |
+    |coffee       |"coffee"        |
+
+  Scenario: User can add a product to the cart
+    Given Open Amazon page
+    When Search for Tritan Farm to Table Pitcher on amazon
+    And Click on the first product
+    And Click on Add to cart button
+    And Open cart page
+    Then Verify cart has 1 item(s)
+
+  Scenario: Verify that user sees hamburger menu on main page
+    Given Open Amazon page
+    Then Verify hamburger menu is present
+
+  Scenario: Verify all footer links are shown
+    Given Open Amazon page
+    Then Verify 38 footer links are shown
