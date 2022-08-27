f1c0a16f668b2e02830f1d408ef28c248deb0fea
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2022-08-27 12:12:34 -0700

    Lesson 10

--- features/environment.py
+++ features/environment.py
@@ -1,37 +1,91 @@
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-
+import allure
+from allure_commons.types import AttachmentType
 from app.application import Application
 
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.events import EventFiringWebDriver
 
-def browser_init(context):
+from support.logger import logger, MyListener
+
+# Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+bs_user = ''
+bs_key = ''
+
+# Allure command:
+# behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/product_page.feature
+
+
+def browser_init(context, test_name):
     """
     :param context: Behave context
+    :param test_name: scenario.name
     """
     context.driver = webdriver.Chrome()
-    # context.browser = webdriver.Safari()
-    # context.browser = webdriver.Firefox()
+    # context.driver = webdriver.Firefox()
+    # context.driver = webdriver.Firefox(executable_path='/Users/svetlanalevinsohn/JobEasy/11-python-selenium-automation/geckodriver')
+    # context.driver = webdriver.Safari()
+
+    ## HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # context.driver = webdriver.Chrome(chrome_options=options)
+
+    # Mobile - run tests on mobile web browser
+    # options = webdriver.ChromeOptions()
+    # mobile_emulation = {"deviceName": "Nexus 5"}
+    # options.add_experimental_option("mobileEmulation", mobile_emulation)
+    # context.driver = webdriver.Chrome(chrome_options=options)
+
+    ### EventFiringWebDriver - log file ###
+    ### for drivers ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(), MyListener())
+    # for headless mode ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(chrome_options = options), MyListener())
+
+    # for browerstack ###
+    # desired_cap = {
+    #     'browser': 'Firefox',
+    #     'os_version': '11',
+    #     'os': 'Windows',
+    #     'name': test_name
+    # }
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     context.driver.maximize_window()
-    context.driver.implicitly_wait(4)
+    context.driver.implicitly_wait(5)
     context.driver.wait = WebDriverWait(context.driver, 10)
+
     context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
-    print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    # print('\nStarted scenario: ', scenario.name)
+    logger.info(f'Started scenario: {scenario.name}')
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
-    print('\nStarted step: ', step)
+    # print('\nStarted step: ', step)
+    logger.info(f'Started step: {step}')
 
 
 def after_step(context, step):
     if step.status == 'failed':
+        logger.error(f'Step failed: {step}')
         print('\nStep failed: ', step)
+        # Mark test case as FAILED on BrowserStack:
+        # context.driver.execute_script(
+        #     'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Step failed"}}')
 
 
 def after_scenario(context, feature):
+    allure.attach(
+        context.driver.get_screenshot_as_png(),
+        name="Screenshot",
+        attachment_type=AttachmentType.PNG
+    )
+
     context.driver.delete_all_cookies()
     context.driver.quit()
