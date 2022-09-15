b6e1b6157a69fcb5e51584170cd0f2808a51ee4e
amir_ali_ansari <amir.ali.hrt@gmail.com>
2022-09-15 18:59:24 -0500

    homework 1

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 driver.maximize_window()
 
 # open the url
