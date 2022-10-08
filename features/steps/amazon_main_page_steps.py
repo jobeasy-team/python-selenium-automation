ab35c8b58a5b235c7ae305bd9545d198aaf716e4
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2022-10-08 12:18:26 -0700

    Lesson 5

--- features/steps/amazon_main_page_steps.py
+++ features/steps/amazon_main_page_steps.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
+from selenium.webdriver.support import expected_conditions as EC
 
 HAM_MENU = (By.ID, 'nav-hamburger-menu')
 FOOTER_LINKS = (By.CSS_SELECTOR, '.navFooterDescItem a')
+SIGN_IN = (By.CSS_SELECTOR, "#nav-signin-tooltip .nav-action-button")
+SEARCH_INPUT = (By.ID, 'twotabsearchtextbox')
 
 
 @given('Open amazon page')
@@ -12,10 +15,16 @@ def open_amazon(context):
 
 @when('Search for {product}')
 def search_product(context, product):
-    element = context.driver.find_element(By.ID, 'twotabsearchtextbox')
+    element = context.driver.find_element(*SEARCH_INPUT)
     element.clear()
     element.send_keys(product)
     context.driver.find_element(By.ID, 'nav-search-submit-button').click()
+
+
+@when('Click on button from SignIn popup')
+def click_sign_in(context):
+    e = context.driver.wait.until(EC.element_to_be_clickable(SIGN_IN), message='Sign in not clickable')
+    e.click()
 
 
 @then('Verify hamburger menu is present')
--- sample_script.py
+++ sample_script.py
@@ -1,26 +1,32 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='/Users/svetlanalevinsohn/JobEasy/12-python-selenium-automation/chromedriver')
 driver.maximize_window()
+driver.implicitly_wait(5)  # applied to find_element(s) / #100 ms if element is found
+driver.wait = WebDriverWait(driver, 10)
 
 # open the url
 driver.get('https://www.google.com/')
 
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Dress')
+search.send_keys('Car')
 
 # wait for 4 sec
-sleep(4)
+# sleep(4)
+btn = (By.NAME, 'btnK')
+driver.wait.until(EC.element_to_be_clickable(btn), message='Search btn not clickable')  # 500 ms
 
 # click search
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify
-assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
