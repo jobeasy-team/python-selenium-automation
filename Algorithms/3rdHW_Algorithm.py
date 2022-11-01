a50d3f9f8a3cfa91217893523b01f5c260743bf0
amir_ali_ansari <amir.ali.hrt@gmail.com>
2022-10-31 22:31:36 -0500

    third homework

--- Algorithms/3rdHW_Algorithm.py
+++ Algorithms/3rdHW_Algorithm.py
@@ -1,4 +1,4 @@
-#>>>>>>>>>>>>>first Excersise<<<<<<<<<<<<
+# >>>>>>>>>>>>>first Excersise<<<<<<<<<<<<
 # using some built-in functions
 # def less_than_arith_mean(lst):
 #     arith_mean=sum(lst)/len(lst)
@@ -9,7 +9,7 @@
 #             less.append(x)
 #             print(less)
 
-#without built-in functions
+# without built-in functions
 # def less_than_arith_mean(lst):
 #     lst_sum=0
 #     lst_len=0
@@ -27,23 +27,26 @@
 #     for y in lst:
 #         if y<lst_mean:
 #          less.append(y)
-#     print("the elements less than arithmatical mean are: "f'{less}')
+#     print("the elements less than arithmetical mean are: "f'{less}')
 
-test_list=[1,4,6,7,8]
+test_list = [1, 4, 6, 7, 8]
+
+
 # print(less_than_arith_mean(test_list))
 
 
-#>>>>>>>>>>>>>Second Excersise<<<<<<<<<<<<
+# >>>>>>>>>>>>>Second Exercise<<<<<<<<<<<<
 
 def two_small_elements(lst):
-    sort=[]
-    #first i sort the numbers from lowest to highest
-    for x in range(0, len(lst)-1):
-        for y in range(x+1, len(lst)):
-            if (lst[x]> lst[y]):
-                lst[y], lst[x]=lst[x], lst[y]
+    # first i sort the numbers from lowest to highest
+    for x in range(0, len(lst) - 1):
+        for y in range(x + 1, len(lst)):
+            if (lst[x] > lst[y]):
+                lst[y], lst[x] = lst[x], lst[y]
         print("sorted list: " f'{lst}')
     # then i will print the first two elements
-    if len(lst)>3:
+    if len(lst) > 3:
         print("the two smallest elements are: " f'{lst[:2]}')
+
+
 print(two_small_elements(test_list))
