d3b17c1dd5aed0b4e86e7a9243f5ae83cec15f13
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2023-02-18 12:25:44 -0800

    Lesson 4

---
+++ features/steps/search_result_steps.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from behave import when, then
+from time import sleep
+
+PRODUCT_PRICE = (By.XPATH, "//div[@data-component-type='s-search-result']//a[.//span[@class='a-price']]")
+
+
+@when('Click on the first product')
+def click_first_product(context):
+    context.driver.find_element(*PRODUCT_PRICE).click()
+    sleep(2)
+
+
+@then('Verify that text {expected_result} is shown')
+def verify_search_result(context, expected_result):
+    actual_result = context.driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
+    assert expected_result == actual_result, f'Expected {expected_result} but got actual {actual_result}'
