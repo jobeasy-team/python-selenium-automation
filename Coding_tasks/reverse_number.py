50cdd51c6219e9d0c05d3d3e5e46b69740993b81
vsupe <vsupez@gmail.com>
2023-02-22 22:54:42 -0500

    Reverse a number

--- Coding_tasks/reverse_number.py
+++ Coding_tasks/reverse_number.py
@@ -1,10 +1,9 @@
 number = 12345
 reversed_array = []
 
-while number!=0:
-    x = number%10
+while number != 0:
+    x = number % 10
     reversed_array.append(x)
-    number = number//10
+    number = number // 10
 
 print(reversed_array)
-
