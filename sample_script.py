ff26f28963d0f91d836f8dd78e1182dc1e3d1494
Ekaterina Suvorova <ekaterinasuvorova@Nikolays-16-Inch-MacBook-Pro-on-Shipping-OS.local>
2023-04-19 15:22:39 -0700

    added chromedriver path

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='/Users/ekaterinasuvorova/Automation 2023/python-selenium-automation/chromedriver')
 driver.maximize_window()
 
 # open the url
